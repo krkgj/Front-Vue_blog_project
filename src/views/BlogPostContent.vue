225b895397cac76963736b970f39242352178d08
Kyeongjin Kim <krkgj0526@naver.com>
2021-09-28 00:41:59 +0900

    20210928-001 본문 router 추가 및 본문 view 추가

--- src/views/BlogPostContent.vue
+++ src/views/BlogPostContent.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-container class="mt-16">
+  <v-container class="my-16">
+    <v-row class="mt-2">
+      <v-card-title class="ma-0 pa-0">{{ postContent.title }}</v-card-title>
+    </v-row>
     <v-row>
-      <v-toolbar-title>{{ postContent.title }}</v-toolbar-title>
+      <v-card-subtitle class="mx-0 px-0 grey--text">
+        작성일 : {{ postContent.createtime }}
+      </v-card-subtitle>
+    </v-row>
+    <v-row>
+      <v-card-text class="mx-0 px-0">{{ postContent.content }}</v-card-text>
     </v-row>
   </v-container>
 </template>
@@ -12,9 +20,10 @@ export default {
   data() {
     return {};
   },
+
   props: {
     postContent: {
-      type: Object,
+      type: JSON,
       default: "",
     },
   },
